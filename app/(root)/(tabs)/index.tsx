e043d53f124441a15d3a201ce17e431ecbc50085
Nam Dang <nathandang73@gmail.com>
2025-03-16 23:31:41 +1100

    Setup routes, nativewind, fonts and assets, appwrite sdk and auth functions

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10">Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/sign-in">Sign In</Link>
+    </View>
+  );
+}
